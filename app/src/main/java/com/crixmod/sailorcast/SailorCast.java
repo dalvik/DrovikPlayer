d0cda5c617a982c40ad26f071aa59507d7ed81fd
Sang kaiyang <sky.xctc@163.com>
2018-10-24 22:54:13 +0800

    debug gsyvideo player
    Signed-off-by: Sang kaiyang <sky.xctc@163.com>

--- app/src/main/java/com/crixmod/sailorcast/SailorCast.java
+++ app/src/main/java/com/crixmod/sailorcast/SailorCast.java
@@ -16,11 +16,16 @@ import com.crixmod.sailorcast.model.upnp.IFactory;
 import com.crixmod.sailorcast.utils.M3UServer;
 import com.drovik.player.AppApplication;
 import com.google.gson.Gson;
+import com.shuyu.gsyvideoplayer.cache.CacheFactory;
+import com.shuyu.gsyvideoplayer.player.PlayerFactory;
 import com.squareup.okhttp.Cache;
 import com.squareup.okhttp.OkHttpClient;
 
 import java.io.File;
 import java.io.IOException;
+
+import tv.danmaku.ijk.media.exo2.Exo2PlayerManager;
+import tv.danmaku.ijk.media.exo2.ExoPlayerCacheManager;
 
 /**
  * Created by fire3 on 14-12-26.
@@ -39,6 +44,9 @@ public class SailorCast extends AppApplication {
     @Override
     public void onCreate() {
         super.onCreate();
+        //LeakCanary.install(this);
+        //PlayerFactory.setPlayManager(Exo2PlayerManager.class);//EXO模式
+        CacheFactory.setCacheManager(ExoPlayerCacheManager.class);//exo缓存模式，支持m3u8，只支持exo
         mContext = this;
         mHttpClient = new OkHttpClient();
         mGson = new Gson();
