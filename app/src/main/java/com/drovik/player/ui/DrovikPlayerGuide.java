ab6883b941b4a1ab8b3a7f19a2d5802ac7caa1b1
Sang kaiyang <sky.xctc@163.com>
2018-11-07 22:05:17 +0800

    add drovik player guide
    Signed-off-by: Sang kaiyang <sky.xctc@163.com>

---
+++ app/src/main/java/com/drovik/player/ui/DrovikPlayerGuide.java
@@ -0,0 +1,46 @@
+package com.drovik.player.ui;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+
+import com.android.audiorecorder.ui.SettingsActivity;
+import com.android.library.ui.activity.BaseGuideActivity;
+import com.drovik.player.R;
+
+public class DrovikPlayerGuide extends BaseGuideActivity {
+
+    private int[] guidePic = new int[3];
+    private String drovikPlayerGuide = "drovik_player_guide";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        SharedPreferences preferences = getSharedPreferences(SettingsActivity.class.getName(), Context.MODE_PRIVATE);
+        if(preferences.getBoolean(drovikPlayerGuide, false)){
+            gotoHome();
+        }
+    }
+
+    @Override
+    public int[] initPictures() {
+        guidePic[0] = R.drawable.drovik_guide_a;
+        guidePic[1] = R.drawable.drovik_guide_b;
+        guidePic[2] = R.drawable.drovik_guide_c;
+        return guidePic;
+    }
+
+    @Override
+    public void gotoNextActivity() {
+        SharedPreferences preferences = getSharedPreferences(SettingsActivity.class.getName(), Context.MODE_PRIVATE);
+        preferences.edit().putBoolean(drovikPlayerGuide, true).apply();
+        gotoHome();
+    }
+
+    private void gotoHome() {
+        Intent intent = new Intent(this, LaunchActivity.class);
+        startActivity(intent);
+        DrovikPlayerGuide.this.finish();
+    }
+}
